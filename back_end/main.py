e45f21e9c54ffb2c4d5c989ba8b9eeb75cb3ac0a
M-Pascal <p.mugisha@alustudent.com>
2025-11-24 01:13:11 +0200

    Fast_API

--- back_end/main.py
+++ back_end/main.py
@@ -1201,6 +1201,6 @@ async def general_exception_handler(_, exc: Exception):
     )
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, log_level="info")
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port, reload=False)
     
